8fcf0398e4db3805029cf3762630492cd987748b
TavaresFr <152510118+TavaresFr@users.noreply.github.com>
2024-11-05 23:30:53 +0000

    Velocity

---
+++ Assets/Time/Script/SecondsUpdate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondsUpdate : MonoBehaviour
+{
+    float timeStartOffset = 0;
+
+    bool gotStartTime = false;
+
+    [SerializeField] float speed = 1f;
+
+    void Update()
+    {
+        if (!gotStartTime)
+        {
+            timeStartOffset = Time.realtimeSinceStartup;
+            gotStartTime = true;
+        }
+        this.transform.position = new Vector3(transform.position.x, transform.position.y, (Time.realtimeSinceStartup - timeStartOffset) * speed);
+    }
+}
